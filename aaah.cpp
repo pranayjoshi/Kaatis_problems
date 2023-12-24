26cbb5932574402505b4964280998420a8fb9699
pranayjoshi <pranayjoshi4466@gmail.com>
2023-12-24 17:25:58 -0600

    Added

---
+++ aaah.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string s, s2;
+    cin >> s;
+    cin >> s2;
+    if (s.size() > s2.size()) {
+        cout << "go" << endl;
+        return 0;
+    }
+    else {
+        cout << "no" << endl;
+        return 0;
+    }
+}
