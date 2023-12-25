1fff435e781a5e18d5997fd095f9d38f5b5996f5
pranayjoshi <pranayjoshi4466@gmail.com>
2023-12-24 21:36:58 -0600

    Added

---
+++ a_towering_problem.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    int n1, n2, n3, n4, n5, n6,s, q;
+    cin >> n1 >> q;
+    }
+
+    return 0;
+}
