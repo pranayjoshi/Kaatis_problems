2fb014fcf9ae8d93b4fbba0775c5f1f964a9ab01
pranayjoshi <pranayjoshi4466@gmail.com>
2023-12-25 16:48:19 -0600

    Added

---
+++ akcija.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_map>
+#include <sstream>
+#include <vector>
+#include <climits>
+#include <iterator>
+using namespace std;
+
+int main()
+{
+    int n;
+    vector<int> lst(n);
+    int s;
+    for(int i = 0; i < n; i++) {
+        cin >> lst[i];
+        s+=lst[i];
+    }
+    sort(lst.rbegin(), lst.rend());
+    int sum = 0;
+    for (int i = 0; i < lst.size(); i++)
+    {
+        if ((i+1)%3 == 0)
+        {
+            sum += lst[i];
+        }
+    }
+    cout << s-sum << endl;
+}
