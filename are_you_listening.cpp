0b564b81f4a282bffc1d616e7a57c709bf5bf735
pranayjoshi <pranayjoshi4466@gmail.com>
2023-12-25 18:30:55 -0600

    Added

--- are_you_listening.cpp
+++ are_you_listening.cpp
@@ -5,11 +5,14 @@ using namespace std;
 int main() {
     int cx, cy, n;
     cin >> cx >> cy >> n;
+    vector<int> pq;
     for (int i = 0; i < n; i++)
     {
         int x, y, r;
         cin >> x >> y >> r;
-        double distance = sqrt(pow(x - cx, 2) + pow(y - cy, 2));
-        cout << distance << endl;
+        int distance = sqrt(pow(x - cx, 2) + pow(y - cy, 2));
+        pq.push_back(distance - r);
     }
+    sort(pq.begin(), pq.end());
+    cout << pq[2] << endl;
 }
