b54a8ce53d4e70a7fc79ecbf98be260f11a64f6b
pranayjoshi <pranayjoshi4466@gmail.com>
2023-12-26 21:36:24 -0600

    Added

--- alien_math.py
+++ alien_math.py
@@ -2,19 +2,18 @@ a = int(input())
 s = list(input().split())
 s2 = input()
 s2 = list(s2)
-print(s2)
 def convertStrToBase(val,  base):
     val.reverse()
     sum = 0
-    for i in range(len(s)-1):
+    for i in range(len(val)):
         sum += val[i] * (base ** i)
     return sum
 val = []
 temp = ""
-for i in range(len(s)-1):
+for i in range(len(s2)):
     temp += s2[0]
     if temp in s:
-        val.append(s.index(temp)+1)
-        s2.remove(temp)
+        val.append(s.index(temp))
+        temp = ""
     s2.pop(0)
 print(convertStrToBase(val, a))
