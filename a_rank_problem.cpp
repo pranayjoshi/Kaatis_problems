2e04a9ebf77677236d943e837c5dbb484117c7b1
pranayjoshi <pranayjoshi4466@gmail.com>
2023-12-27 14:45:36 -0600

    Added

---
+++ a_rank_problem.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n, m;
+    vector<int> last(n,0);
+    for(int i = 0; i < n; i++) {
+        last[i] = i;
+    }
+
+    return 0;
+}
