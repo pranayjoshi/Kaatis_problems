5c605fb22fccde9835a2214574f0d5ea691c86da
pranayjoshi <pranayjoshi4466@gmail.com>
2023-12-28 16:02:55 -0600

    Added

--- addemup.cpp
+++ addemup.cpp
@@ -7,23 +7,35 @@ using namespace std;
 #define ar array
 #define ll long long
 
+bool inVector (vector<int> v, int x) {
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] == x) {
+            return true;
+        }
+    }
+    return false;
+}
+
 void solve() {
     int n; ll sum;
     cin>>n>>sum;
-    stack<int> s;
-    
+    vector<int> s;
+    bool st = false;
     ll arr[n];
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
     for (int i=0; i < n; i++){
-        if(arr[i]+arr[i-1]>x){
-            i--;
-        }else{
+        if (inVector(s, arr[i])) {
+            st = true;
             break;
         }
+        else {
+            int diff = sum - arr[i];
+
+        }
     }
-    cout<<(i+1)<<endl;
+    cout<<(1)<<endl;
 }
 
 int main() {
