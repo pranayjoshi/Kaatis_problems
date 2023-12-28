546a2d97060f1a34d4e2ed0cd5f5342b0895fdcf
pranayjoshi <pranayjoshi4466@gmail.com>
2023-12-28 02:19:49 -0600

    Added

---
+++ appaling_architecture.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int h, w;
+    cin >> h >> w;
+    vector<string> grid(h);
+    for (int i = 0; i < h; i++) {
+        cin >> grid[i];
+    }
+
+    double total_mass = 0;
+    double total_moment = 0;
+    int leftmost = w;
+    int rightmost = 0;
+
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            if (grid[i][j] != '.') {
+                total_mass++;
+                total_moment += j;
+                leftmost = min(leftmost, j);
+                rightmost = max(rightmost, j);
+            }
+        }
+    }
+
+    double center_of_gravity = total_moment / total_mass;
+
+    if (center_of_gravity < leftmost) {
+        cout << "left" << endl;
+    } else if (center_of_gravity > rightmost) {
+        cout << "right" << endl;
+    } else {
+        cout << "balanced" << endl;
+    }
+
+    return 0;
+}
