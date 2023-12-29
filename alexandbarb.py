24606db728d148dfd45070c8426ab9dffe2dba88
pranayjoshi <pranayjoshi4466@gmail.com>
2023-12-29 01:56:02 -0600

    Added

--- alexandbarb.py
+++ alexandbarb.py
@@ -1,13 +1,5 @@
 n, a, b = map(int, input().split())
-player = True
-
-while n > 0:
-    if (n >= a) if player else (n >= b):
-        n -= a if player else b
-    else:
-        break
-
-    player = not player
-
-winner = "Barb" if player else "Alex"
-print(winner)
+if a % (b + 1) == 0 or (n % (a + b)) % (a + b) < a:
+    print("Barb")
+else:
+    print("Alex")
