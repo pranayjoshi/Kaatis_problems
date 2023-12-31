8f99e64fb755b1e27a7c9e433de77e3ba67f53eb
pranayjoshi <pranayjoshi4466@gmail.com>
2023-12-30 21:56:55 -0600

    Added

--- alice.cpp
+++ alice.cpp
@@ -1,50 +1,52 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+#include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
-int maxWeight(vector<int> &arr, int k)
-{
-    int maxWeight = 0;
-    int n = arr.size();
-    int s = 0;
+#define ar array
+#define ll long long
 
-    for (int i = 0; i < n;)
-    {
-        int weight = 0;
-        while (i < n && arr[i] >= k)
-        {
-            if (arr[i] == k)
-                s++;
-            
-            weight += arr[i];
-            if (arr[i] == k)
-                k--;
-            
-            i++;
-        }
-        maxWeight = max(maxWeight, weight);
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> arr(n);
+    for (int &v : arr) {
+        cin >> v;
     }
 
-    return maxWeight;
+    int curr = 0;
+    int max_sum = 0;
+    while (curr < n) {
+        if (arr[curr] == m) {
+            int left = curr - 1;
+            int tot = m;
+            while (left >= 0 && arr[left] > m) {
+                tot += arr[left];
+                left--;
+            }
+            int right = curr + 1;
+            while (right < n && arr[right] > m) {
+                tot += arr[right];
+                right++;
+            }
+            max_sum = max(max_sum, tot);
+            curr = right;
+        } else {
+            curr++;
+        }
+    }
+    cout << max_sum << endl;
 }
 
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
 
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n, k;
-        cin >> n >> k;
-        vector<int> arr(n);
-        for (int i = 0; i < n; i++)
-        {
-            cin >> arr[i];
-        }
-        cout << maxWeight(arr, k) << "\n";
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t  << ": ";
+        solve();
     }
-    return 0;
 }
