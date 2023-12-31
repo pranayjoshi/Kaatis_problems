5b62275a0e61a66ec5903a4d3644bfdc02bc4663
pranayjoshi <pranayjoshi4466@gmail.com>
2023-12-30 21:38:51 -0600

    Added

--- alternative_anagram.cpp
+++ alternative_anagram.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <map>
+#include <set>
 
 using namespace std;
 
@@ -12,18 +13,22 @@ string rearrangeString(string s) {
         substrings.insert(s.substr(i, n/2));
     }
     if (substrings.size() == n - n/2 + 1) return s;
+
     map<char, int> freq;
     for (char c : s) {
         freq[c]++;
         if (freq[c] > (n + 1) / 2) return "-1";
     }
 
-    vector<pair<int, char> > freqVec;
+    vector<pair<int, char>> freqVec;
     for (auto& it : freq) {
-        freqVec.push_back(make_pair(it.second, it.first));
+        freqVec.push_back({it.second, it.first});
     }
 
-    sort(freqVec.rbegin(), freqVec.rend());
+    sort(freqVec.begin(), freqVec.end(), [](const pair<int, char>& a, const pair<int, char>& b) {
+    if (a.first != b.first) return a.first > b.first;
+    return a.second < b.second;
+});
 
     string res(n, ' ');
     int i = 0;
@@ -35,6 +40,13 @@ string rearrangeString(string s) {
             if (i >= n) i = (i + 1) % (n / 2);
         }
     }
+
+    // Check if the rearranged string meets the condition
+    substrings.clear();
+    for (int i = 0; i <= n - n/2; i++) {
+        substrings.insert(res.substr(i, n/2));
+    }
+    if (substrings.size() < n - n/2 + 1) return "-1";
 
     return res;
 }
