98685d2c4245421e520d68837fa2c0f520a1022a
pranayjoshi <pranayjoshi4466@gmail.com>
2023-12-30 21:55:19 -0600

    Added

--- alice.cpp
+++ alice.cpp
@@ -4,31 +4,44 @@
 
 using namespace std;
 
-int maxWeight(vector<int>& arr, int k) {
+int maxWeight(vector<int> &arr, int k)
+{
     int maxWeight = 0;
     int n = arr.size();
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == k) {
-            int weight = 0;
-            while (i < n && arr[i] >= k) {
-                weight += arr[i];
-                if (arr[i] == k) k--;
-                i++;
-            }
-            maxWeight = max(maxWeight, weight);
+    int s = 0;
+
+    for (int i = 0; i < n;)
+    {
+        int weight = 0;
+        while (i < n && arr[i] >= k)
+        {
+            if (arr[i] == k)
+                s++;
+            
+            weight += arr[i];
+            if (arr[i] == k)
+                k--;
+            
+            i++;
         }
+        maxWeight = max(maxWeight, weight);
     }
+
     return maxWeight;
 }
 
-int main() {
+
+int main()
+{
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         int n, k;
         cin >> n >> k;
         vector<int> arr(n);
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             cin >> arr[i];
         }
         cout << maxWeight(arr, k) << "\n";
