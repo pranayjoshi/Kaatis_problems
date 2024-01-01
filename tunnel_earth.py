9982b1d2108038238204115eb51e66e79795fdec
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-01 13:39:53 -0600

    addeD:

--- tunnel_earth.py
+++ tunnel_earth.py
@@ -1,14 +1,15 @@
 import math
 
-R = 6371000
+R = 6371000  # radius of the Earth in meters
 
 def to_rad(deg):
     return deg * math.pi / 180
 
 def distance(lat1, lon1, lat2, lon2):
     lat1, lon1, lat2, lon2 = map(to_rad, [lat1, lon1, lat2, lon2])
-    d_great_circle = R * math.acos(math.sin(lat1) * math.sin(lat2) + math.cos(lat1) * math.cos(lat2) * math.cos(lon2 - lon1))
-    d_straight_line = 2 * R * math.asin(math.sqrt(math.sin((lat2 - lat1) / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin((lon2 - lon1) / 2) ** 2))
+    d = math.acos(math.sin(lat1) * math.sin(lat2) + math.cos(lat1) * math.cos(lat2) * math.cos(lon2 - lon1))  # angular distance
+    d_great_circle = R * d
+    d_straight_line = 2 * R * math.sin(d / 2)
     return d_great_circle - d_straight_line
 
 t = int(input())
