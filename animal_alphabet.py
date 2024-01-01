33f603d7ed24c50d2bdaf151c6b87634d3125247
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-01 14:08:50 -0600

    addeD:

---
+++ animal_alphabet.py
@@ -0,0 +1,23 @@
+a = input()
+l = []
+t = int(input())
+for i in range(t):
+    s = input()
+    l.append(s)
+out = []
+for i in l:
+    if i[0] == a[-1]:
+        out.append(i)
+if len(out) == 0:
+    print("?")
+else:
+    for i in out:
+        q = True
+        for j in l:
+            if j[0] == i[-1]:
+                q = False
+        if q == False:
+            print(i)
+            break
+    else:
+        print(out[0])
