b9315efcee6a596d6e84929c35ab4a6ec263a9a1
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-04 19:14:49 -0600

    added

--- 99prob2.cpp
+++ 99prob2.cpp
@@ -1,1 +1,15 @@
-#include <iostream
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int n, q;    
+    cin >> n, q;
+    // int array using cin
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+}
