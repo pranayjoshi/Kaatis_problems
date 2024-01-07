2bd7902a7abdee35ee2197a2472985b20f82c86c
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-07 14:57:27 -0600

    added

---
+++ bank.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    unordered_map<int, int> map;
+    for (int i = 0; i < a; i++) {
+        int ci, ti;
+        cin >> ci >> ti;
+        if (ti <= b) {
+            map[ti] <
+        }
+    }
+}
