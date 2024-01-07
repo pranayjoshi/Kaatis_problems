409ff61a19cdcf45c83ab271a8d2798f00a10215
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-06 21:11:10 -0600

    added

--- alphabet.cpp
+++ alphabet.cpp
@@ -5,4 +5,23 @@ using namespace std;
 int main(){
     string n;
     cin >> n;
+    unordered_map<char, int> m;
+    // add all alphabet to map
+    int count = 0;
+    for (int i = 0; i < 26; i++) {
+        m['a' + i] = 0;
+    }
+    for (int i = 0; i < n.size(); i++) {
+        m[n[i]]++;
+    }
+    for (int i = 0; i < 26; i++) {
+        if (m['a' + i] == 0) {
+            count++;
+        }
+        if (m['a' + i] > 1) {
+            count+= m['a' + i] - 1;
+        }
+    }
+    cout << count << endl;
+
 }
