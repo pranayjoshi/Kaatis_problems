dfd8260f891348f196dc422e47379d403ca855b3
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-07 14:46:36 -0600

    added

---
+++ bard.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a;
+    cin >> b;
+    unordered_map<int, int> map;
+    for (int i = 0; i < b; i++) {
+        int x;
+        cin >> x;
+        vector<int> v;
+        for (int i = 0; i < x; i++) {
+            cin >> v[i];
+        }
+        map[x] = i;
+    }
+}
