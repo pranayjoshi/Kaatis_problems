1cda5b2d32226c1d725a3aa25d4f5e6e7b44df66
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-06 20:55:24 -0600

    added

--- basicp2.py
+++ basicp2.py
@@ -1,43 +1,56 @@
 a, b = map(int, input().split())
 lst = list(map(int, input().split()))
+
 if b == 1:
-    diff = []
+    diff = set()
     s = True
     for i in lst:
         if i in diff:
             s = False
             print("Yes")
             break
-        diff.append(7777 - i)
-    if s: print("No")
+        diff.add(7777 - i)
+    if s:
+        print("No")
+
 elif b == 2:
-    diff = []
+    diff = set()
     s = True
     for i in lst:
         if i in diff:
             s = False
             print("Contains duplicate")
             break
-        diff.append(i)
-    if s: print("Unique")
+        diff.add(i)
+    if s:
+        print("Unique")
+
 elif b == 3:
     diff = {}
     s = True
     for i in lst:
-        if i in diff: diff[i]+=1
-        else: diff[i]=1
-            
-        if diff[i] > len(lst)/2:
+        if i in diff:
+            diff[i] += 1
+        else:
+            diff[i] = 1
+
+        if diff[i] > len(lst) / 2:
             s = False
             print(i)
             break
-    if s: print(-1)
+    if s:
+        print(-1)
+
 elif b == 4:
     lst.sort()
-    if (len(lst)%2==0): print(lst[len(lst)//2-1], lst[len(lst)//2])
-    else: print(lst[len(lst)//2])
+    if len(lst) % 2 == 0:
+        print(lst[len(lst) // 2 - 1], lst[len(lst) // 2])
+    else:
+        print(lst[len(lst) // 2])
+
 elif b == 5:
     lst.sort()
     for i in lst:
-        if 100 <= i <= 999: print(i, end=" ")
+        if 100 <= i <= 999:
+            print(i, end=" ")
     print()
