6acbc87c2c6e805ea7c4bc97be4f128590ef5e9f
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-06 20:40:59 -0600

    added

--- basicp2.py
+++ basicp2.py
@@ -2,30 +2,31 @@ a, b = map(int, input().split())
 lst = list(map(int, input().split()))
 if b == 1:
     diff = []
-    s = False
+    s = True
     for i in lst:
         if i in diff:
-            s = True
+            s = False
             print("Yes")
             break
         diff.append(7777 - i)
     if s: print("No")
 elif b == 2:
     diff = []
-    s = False
+    s = True
     for i in lst:
         if i in diff:
-            s = True
+            s = False
             print("Unique")
             break
         diff.append(i)
     if s: print("Contains duplicate")
 elif b == 3:
     diff = {}
+    s = True
     for i in lst:
         diff[i]+=1
         if diff[i] > len(lst/2):
-            s = True
+            s = False
             print(i)
             break
     if s: print(-1)
@@ -38,3 +39,13 @@ if b == 5:
     aj = []
     for i in lst:
         if 100 <= i <= 999: print(i, end=" ")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
