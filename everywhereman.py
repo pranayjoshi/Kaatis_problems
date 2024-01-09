b04e1307f3697fa6e1b02ead4ec86298ee416312
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-09 01:15:10 -0600

    Added

--- everywhereman.py
+++ everywhereman.py
@@ -1,13 +1,14 @@
-for i in range(int(input())):
+for _ in range(int(input())):
     a = {}
     s = int(input())
-    for i in range(s):
-        if i not in a:
-            a[i] = 1
+    for _ in range(s):  
+        num = int(input())  
+        if num not in a:
+            a[num] = 1
         else:
-            a[i]+=1
+            a[num] += 1
     count = 0
-    for key,val in a.items():
+    for val in a.values(): 
         if val == 1:
-            count +=1
+            count += 1
     print(count)
