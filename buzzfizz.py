d4087cf90e26393c6c35b5bfb4a3520810d40827
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-09 00:29:13 -0600

    Added

--- buzzfizz.py
+++ buzzfizz.py
@@ -1,14 +1,15 @@
 import math
-
+def lcm(x, y):
+    return x * y // math.gcd(x, y)
 a, b, c, d = map(int, input().split())
 co = 0
 
 if c == 0 or d == 0:
     print(0)
 else:
-    g = math.lcm(c, d)
+    g = lcm(c, d)
 
     co = (b - a+1) // g
-    if (a%g==0 and b%g==0): co+=1
+    if (a%g and b%g==0): co+=1
 
     print(co)
