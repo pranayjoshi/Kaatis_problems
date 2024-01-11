12265384f9eab0cb98c43de2fcf1965bf22aee91
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-10 21:11:14 -0600

    Added

--- fluortan.py
+++ fluortan.py
@@ -1,7 +1,10 @@
-n = int(input())
-happiness = list(map(int, input().split()))
-happiness.sort(reverse=True)
-dp = [0]*(n+1)
-for i in range(1, n+1):
-    dp[i] = max(dp[i-1], happiness[i-1] + dp[i-2])
-print(dp[n])
+def max_sum_after_moving_zero(arr):
+    arr.sort(reverse=True)
+    total = 0
+    for i in range(len(arr)):
+        total += i * arr[i]
+    return total
+
+# Test the function
+arr = [2,-4,5,-3, 0, 1, 2]
+print(max_sum_after_moving_zero(arr))  # Output: 4
