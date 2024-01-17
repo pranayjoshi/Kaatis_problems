0a6d9ac66e77cbc00c21184e3bf0b938984eb7cf
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-16 19:47:54 -0600

    added

--- gullilime.py
+++ gullilime.py
@@ -1,16 +1,16 @@
 a = int(input())
-str = input()
-str = str[::-1]
+st = input()
+st = st[::-1]
 g = 0
 a = 0
-if str[0] == 'G': g += 1
+if st[0] == 'G': g += 1
 else: a += 1
 gm = g
 am = a
-for i in range(1,len(str)):
-    if str[i] == 'G': g += 1
-    elif str[i] == 'A': a += 1 
-    if (g-a) > gm:
+for i in range(1,len(st)):
+    if st[i] == 'G': g += 1
+    elif st[i] == 'A': a += 1 
+    if ((g-a)/a) > (gm-am)/am:
         gm = g
         am = a
 print(str(gm)+"-"+str(am))
