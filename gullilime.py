5790691c8fbf10da809bd7713bbbaf1513a2d284
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-16 19:39:34 -0600

    added

--- gullilime.py
+++ gullilime.py
@@ -7,9 +7,9 @@ if str[0] == 'G': g += 1
 else: a += 1
 gm = g
 am = a
-for i in range(1,len(str)+1):
+for i in range(1,len(str)):
     if str[i] == 'G': g += 1
-    else: a += 1
+    elif str[i] == 'A': a += 1
     if (g-a) > gm:
         gm = g-a
         am = a
