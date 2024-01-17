e5dde5c19c819fe03ad3203212bb16516b143c93
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-16 20:10:10 -0600

    added

--- gullilime.py
+++ gullilime.py
@@ -10,7 +10,7 @@ am = a
 for i in range(1,len(st)):
     if st[i] == 'G': g += 1
     elif st[i] == 'A': a += 1 
-    if a + g != 0 and ((g - a) / (a + g)) > ((gm - am) / (am + gm)):
+    if a + g != 0 and (am + gm != 0) and ((g - a) / (a + g)) > ((gm - am) / (am + gm)):
         gm = g
         am = a
 print(str(gm)+"-"+str(am))
