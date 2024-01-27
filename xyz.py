9a40183b3f0979f90f71c133ea747451dcb20ffb
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-27 11:36:56 -0600

    added

--- xyz.py
+++ xyz.py
@@ -2,12 +2,14 @@ black = []
 white = []
 for i in range(17):
     a = input()
-    dict = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H":8}
+    dict = {1: "a", 2: "b", 3: "c", 4: "d", 5: "e", 6:"f", 7:"g", 8:"h"}
     if a[0] != "+":
         lst = list(a.split("|"))
         lst[0] = ".."
+        lst.pop(-1)
         print(lst)
         for j in range(len(lst)):
             if lst[j][1].isalpha():
                 str = lst[j][1].upper()+ str(dict[j]) +str(i//2+1)
-                # black.append()
+                black.append()
+            pass
